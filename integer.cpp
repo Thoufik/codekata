2894787019624881d8895368d3f91d55c91c6dd9
thoufikhussan <36732073+thoufikhussan@users.noreply.github.com>
2018-02-23 18:02:27 +0530

    Create integer.cpp

---
+++ integer.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main()
+{
+int a=5,b=2,sum=0;    		
+rintf("Enter two numbers..."); 
+scanf("%d%d",&a,&b);       
+sum=a+b;                   
+printf("The sum is %d",sum);
+return 0;
+}
