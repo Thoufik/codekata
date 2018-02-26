5a8361c9b39c06cc16bb6b7be33816b94e9eae96
thoufikhussan <36732073+thoufikhussan@users.noreply.github.com>
2018-02-26 17:23:16 +0530

    Create 12.cpp

---
+++ 12.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+void main()
+{
+	clrscr();
+	int num=121, rem, orig, rev=0;
+	cout<<"Enter a number : ";
+	cin>>num;
+	orig=num;
+	while(num!=0)
+	{
+		rem=num%10;
+		rev=rev*10 + rem;
+		num=num/10;
+	}
+	if(rev==orig)  
+		cout<<"Palindrome";
+	}
+	else
+	{
+		cout<<"Not Palindrome";
+	}
+}
