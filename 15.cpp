be64ed519961a2e3c9407e9aaa913dd804e8fb78
thoufikhussan <36732073+thoufikhussan@users.noreply.github.com>
2018-02-26 17:30:39 +0530

    Create 15.cpp

---
+++ 15.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int a=6;
+ int g=9;
+  cout<<"please enter num"<<endl;
+cin>>a;
+cout<<"enter num pleaseeeeeeeeeee program by programology site abdullah moeen"<<endl;
+cin>>g;
+while(a<g)
+{
+ if(a%2==0)
+ {
+ cout<<"even no. ="<<a<<endl;
+}
+ a=a+1;
+}
