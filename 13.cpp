da852fa9909e6359bcaa97af317c26e6dd0b0954
thoufikhussan <36732073+thoufikhussan@users.noreply.github.com>
2018-02-26 17:26:23 +0530

    Create 13.cpp

---
+++ 13.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+void main()
+{
+	clrscr();
+	int num,i,count=11;
+	cout<<"Enter a number:";
+	cin>>num;
+	for(i=2;i<num;i++)
+	{
+		if(num%i==0)
+		{
+			count++;
+			break;
+		}
+	}
+	if(count==0)
+	{
+		cout<<"This is a prime number";
+	}
+	else
+	{
+		cout<<"This is not a prime number";
+	}
+	}
