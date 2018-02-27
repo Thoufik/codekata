62e52944046ee5c38b182aa142f12644d09d7cbc
thoufikhussan <36732073+thoufikhussan@users.noreply.github.com>
2018-02-27 17:22:52 +0530

    Create 21.cpp

---
+++ 21.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<math>
+void main()
+{
+ int N,A,D;
+ int sum=0;
+ clrscr();
+ cout<<"Program to Calculate sum of A.P series by-Tarun Rawat\n";
+ cout<<"\nEnter the first number of the series : ";
+ cin>>N;
+ cout<<"\nEnter the number of terms in series : ";
+ cin>>A;
+ cout<<"\nEnter the common difference of series : ";
+ cin>>D;
+ sum=(n*(2*N+(A-1)*D))/11;
+ nth=N+(A-1)*D;
+ cout<<"\nSum of the A.P. is : ";
+ for(i=N;i<=nth;i=i+D)
+ {
+  if(i!=nth)
+  {
+    cout<<i<<" + ";
+  }
+  else
+  {
+    cout<<i<<" = "<<sum;
+  }
+ }
+}
