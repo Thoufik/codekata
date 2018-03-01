52ede1650e34df6b5712f5bf556bcdcd2d280d3a
thoufikhussan <36732073+thoufikhussan@users.noreply.github.com>
2018-03-01 17:11:34 +0530

    Create 31.cpp

---
+++ 31.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() 
+{
+	string name_surname=" Thoufik hussan";
+		cout<<"length of your name and surname is:"<<" "<<name_surname.size()<<endl;
+		return 0;
+}
