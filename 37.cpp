d65ba20f246e91a42d06ee23900c331442a223af
thoufikhussan <36732073+thoufikhussan@users.noreply.github.com>
2018-03-02 17:17:53 +0530

    Create 37.cpp

---
+++ 37.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 7, b = 5, temp;
+    cout << "Before swapping." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+    temp = a;
+    a = b;
+    b = temp;
+    cout << "\nAfter swapping." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+    return 0;
+}
