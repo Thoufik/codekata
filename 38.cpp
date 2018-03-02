37e7f96e4100fba7e74640f59b08d454fc11c0c6
thoufikhussan <36732073+thoufikhussan@users.noreply.github.com>
2018-03-02 17:23:36 +0530

    Create 38.cpp

---
+++ 38.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+     int a=76;
+    int b=80;
+    cout << "First value is "<< a <<endl;
+    cout << "Second value is "<< b <<endl;
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+    cout <<"== After Swapping ==" <<endl;
+    cout << "First value is "<< a <<endl;
+    cout << "Second value is "<< b <<endl;
+	return 0;
+    
+}
