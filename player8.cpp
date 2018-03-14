2f82977116cfad8b347a2a575aca742465598072
thoufikhussan <36732073+thoufikhussan@users.noreply.github.com>
2018-03-14 17:45:38 +0530

    Create player8.cpp

---
+++ player8.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+string convert(string s)
+{
+    int n = s.length();
+int res_ind = 0;
+ for (int i = 0; i < n; i++) 
+    {
+        if (s[i] == ' ')
+        {
+            s[i + 1] = toupper(s[i + 1]);
+            continue;
+        }
+        else
+            s[res_ind++] = s[i];        
+    }
+ 
+    s[res_ind] = '\0';
+ 
+    // return string to main
+    return s;
+}
+ 
+// Driver program
+int main()
+{
+    string str = "I get intern at geeksforgeeks";
+    cout << convert(str);
+    return 0;
+}
