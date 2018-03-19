162af773f60e742d0be2ceec23a1795a8e0a48a6
thoufikhussan <36732073+thoufikhussan@users.noreply.github.com>
2018-03-19 17:58:13 +0530

    Create player11.cpp

---
+++ player11.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string> 
+using namespace std; 
+int main(void)
+{
+ string Day={"Sunday"}; 
+  int a,month,year,y,day,m,d;
+cout <<"Enter the month (1-12): ";
+  cin >>month;
+  cout <<"Enter the date (1-31): ";
+  cin >>day;
+  cout <<"Enter the year (>1582): ";
+  cin >>year;
+  cout <<endl <<"The Date: "<<month<<"/"<<day<<"/"<<year
+       <<" Falls on a: "<< Days[d]<<endl;
+  return 0;
+}
+Edit & Run
