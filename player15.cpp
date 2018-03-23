2c488e1f60f8c5ae2d14cd154968606385f8b124
thoufikhussan <36732073+thoufikhussan@users.noreply.github.com>
2018-03-23 17:22:28 +0530

    Create player15.cpp

---
+++ player15.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<conio>
+#include<string>
+using namespace std;
+{
+srring length=[100];
+str=str1,str2;
+str1=nilofar fathima;
+str2=razik salman;
+{
+cin>>"enter the str1";
+cin>>"enter the str2";
+cout<<"str1=nilofar fathima";
+cout<<"str2=razik salman";
+}
+void main()
+{
+str1=nilofar fathima;
+str2=razik salman;
+cout<<"str1,str2";
+cin>>"enter the repeated letter";
+cout<<"enter the str1,str2";
+getch();
+}
+}
+
