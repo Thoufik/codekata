6af8f4303952797f214c018d099a9f4ac078b44f
thoufikhussan <36732073+thoufikhussan@users.noreply.github.com>
2018-03-23 17:43:21 +0530

    Create player16.cpp

---
+++ player16.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<conio>
+using namespace std;
+{
+int n=75;
+array[75];
+cout<<"enter the numbers";
+{
+cin>>"enter the elements";
+cin>>"print the array of elements";
+array[75]={0,1,3,5,8,6,7,7,6,5,8,1,0,3};
+cout<<"enter the array";
+}
+void main()
+{
+num={0,1,3,5,8,6,7,7,6,5,8,1,0,3};
+cout<<"enter one number";
+cout <<"print the repeated number";
+getch();
+}
