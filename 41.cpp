33ce367e840bc89a6aae9300280d26d44fbe65b7
Thoufik <36663603+Thoufik@users.noreply.github.com>
2018-05-25 13:50:41 +0530

    Create 41.cpp

---
+++ 41.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+void printNTimes(char c, int n)
+{
+    cout << string(n, c) << endl;
+}
+int main()
+{
+    int n = 6;
+    char c = 'computer science and engineering';
+    printNTimes(c, n);
+  return 0; 
+}
