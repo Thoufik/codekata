3f165d76701871966030c8905c53b79b1d2c62fe
Thoufik <36663603+Thoufik@users.noreply.github.com>
2018-05-25 14:53:27 +0530

    Create 45.cpp

---
+++ 45.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int no,a=76;
+clrscr();
+cout<<"Enter the num : ";
+cin>>no;
+while(no>0)
+{
+no=76;
+a++;
+}
+cout<<"\n no. of digits in given number is: "<<a;
+getch();
+}
